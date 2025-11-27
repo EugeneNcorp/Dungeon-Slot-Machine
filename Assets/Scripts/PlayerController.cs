cef3807c0fcd95a7785ad4e578e90a70687e6f2e
Eugene Nazarenko <nazareu@proton.me>
2025-11-26 22:40:12 -0700

    Character Controller ver1
    Player can move and rotate camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float speed = 12.0f;
+
+    public CharacterController controller;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        Vector3 moveNormal = move * speed * Time.deltaTime;
+        controller.Move(moveNormal);
+    }
 }
